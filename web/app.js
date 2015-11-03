13f017c3f3a49fe21d33d53937cc34a2b546485f
wertrain <wertrain@gmail.com>
2015-11-03 18:11:01 +0900

    refactor

--- web/app.js
+++ web/app.js
@@ -1,51 +1,65 @@
-﻿var MainController = function($http) {
-    this.searchKeyword = "プログラミング";
-    this.sortSelectMenu = [
-        { num:1, label:"関連度", tag:"relevance" }, 
-        { num:2, label:"閲覧数", tag:"mostviewed" },
-        { num:3, label:"ダウンロード数", tag:"mostdownloaded" },
-        { num:4, label:"最新", tag:"latest" },
-    ];
-    this.selectedSort = this.sortSelectMenu[1];
-    this.isString = angular.isString;
-    this.slideShareData = null;
-    this.$http = $http;
-};
+﻿(function() {
+    'use strict';
+    
+    var module = angular.module('Controllers', []);
+    var app = angular.module('App', [
+        'Controllers'
+    ]);
 
-MainController.prototype = {
-    init: function() {
-        this.getSlideShareData();
-    },
-    getSlideShareData: function() {
-        var q = encodeURI(this.searchKeyword);
-        var that = this;
-        this.$http.get('php/search.php?keyword='+ q + '&sort=' + this.selectedSort.tag).
-        success(function(response){
-            that.slideShareData = response;
-            console.log(that.slideShareData);
-        }).
-        error(function(data, status, headers, config) {
-            alert("Error: getSlideShareData");
-        });
+    module.controller('MainController', [
+        '$http', 
+        MainController
+    ]);
+
+    function MainController(
+        $http
+    ){
+        this.$http = $http;
+        
+        this.searchKeyword = 'プログラミング';
+        this.sortSelectMenu = [
+            { num:1, label:'関連度', tag:'relevance' }, 
+            { num:2, label:'閲覧数', tag:'mostviewed' },
+            { num:3, label:'ダウンロード数', tag:'mostdownloaded' },
+            { num:4, label:'最新', tag:'latest' },
+        ];
+        this.selectedSort = this.sortSelectMenu[1];
+        this.isString = angular.isString;
+        this.slideShareData = null;
     }
-}
-
-angular.module('App', [])
-.controller('MainController', ['$http', MainController])
-.filter('truncate', function () {
-    return function (text, length, suffix) {
-        if (typeof text === 'undefined') {
-            text = '';
-        }        
-        if (isNaN(length)) {
-            length = 10;
+    MainController.prototype = {
+        init: function() {
+            this.getSlideShareData();
+        },
+        getSlideShareData: function() {
+            var q = encodeURI(this.searchKeyword);
+            var that = this;
+            this.$http.get('php/search.php?keyword='+ q + '&sort=' + this.selectedSort.tag).
+            success(function(response){
+                that.slideShareData = response;
+                console.log(that.slideShareData);
+            }).
+            error(function(data, status, headers, config) {
+                alert('Error: getSlideShareData');
+            });
         }
-        if (typeof suffix === 'undefined') {
-            suffix = '...';
-        }
-        if (text.length > length) {
-            return String(text).substring(0, length) + suffix;
-        }
-        return text;
     };
-});
+    
+    app.filter('truncate', function () {
+        return function (text, length, suffix) {
+            if (typeof text === 'undefined') {
+                text = '';
+            }        
+            if (isNaN(length)) {
+                length = 10;
+            }
+            if (typeof suffix === 'undefined') {
+                suffix = '...';
+            }
+            if (text.length > length) {
+                return String(text).substring(0, length) + suffix;
+            }
+            return text;
+        };
+    });
+})();
